bdd1bc16e9fa9fb6b86e0ed36c32c33d295ff1e2
Pamela Bortnick <pbortnick@gmail.com>
2022-07-05 16:29:11 -0400

    Set up homepage and use cases page

--- src/layouts/_proxied-dot-io/vagrant/index.tsx
+++ src/layouts/_proxied-dot-io/vagrant/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import HashiHead from '@hashicorp/react-head'
-import HashiStackMenu from '@hashicorp/react-hashi-stack-menu'
 import AlertBanner from '@hashicorp/react-alert-banner'
 import Min100Layout from '@hashicorp/react-min-100-layout'
 import useProductMeta, { Products } from '@hashicorp/platform-product-meta'
@@ -11,6 +10,18 @@ import localConsentManagerServices from 'lib/consent-manager-services/io-sites'
 import Footer from 'components/_proxied-dot-io/vagrant/footer'
 import ProductSubnav from 'components/_proxied-dot-io/vagrant/subnav'
 import productData from 'data/vagrant.json'
+import query from './query.graphql'
+
+interface Props {
+	/**
+	 * Data from data which may contain nav items for the use cases nav
+	 */
+	data: {
+		useCaseNavItems: Array<{ url: string; text: string }>
+	}
+	/** Page contents to render in the layout */
+	children: React.ReactNode
+}
 
 const { ConsentManager, openConsentManager } = createConsentManager({
 	segmentWriteKey: productData.analyticsConfig.segmentWriteKey,
@@ -18,17 +29,13 @@ const { ConsentManager, openConsentManager } = createConsentManager({
 	otherServices: [...localConsentManagerServices],
 })
 
-function VagrantIoLayout({
-	children,
-}: {
-	/** Page contents to render in the layout */
-	children: React.ReactNode
-}): React.ReactElement {
+function VagrantIoLayout({ children, data }: Props): React.ReactElement {
 	usePageviewAnalytics({
 		siteId: process.env.NEXT_PUBLIC_FATHOM_SITE_ID_VAGRANT,
 		includedDomains: productData.analyticsConfig.includedDomains,
 	})
 	const { themeClass } = useProductMeta(productData.name as Products)
+	const { useCaseNavItems } = data
 
 	return (
 		<>
@@ -49,13 +56,57 @@ function VagrantIoLayout({
 						hideOnMobile
 					/>
 				)}
-				<HashiStackMenu onPanelChange={() => null} />
-				<ProductSubnav />
+				<ProductSubnav
+					menuItems={[
+						{
+							text: 'Overview',
+							url: '/',
+							type: 'inbound',
+						},
+						'divider',
+						{
+							text: 'Use Cases',
+							submenu: [
+								...useCaseNavItems.map((item) => {
+									return {
+										text: item.text,
+										url: `/use-cases/${item.url}`,
+									}
+								}),
+							].sort((a, b) => a.text.localeCompare(b.text)),
+						},
+						{
+							text: 'Tutorials',
+							url: 'https://learn.hashicorp.com/packer',
+							type: 'inbound',
+						},
+						{
+							text: 'Docs',
+							url: '/docs',
+							type: 'inbound',
+						},
+						{
+							text: 'API',
+							url: '/api-docs',
+							type: 'inbound',
+						},
+						{
+							text: 'Community',
+							url: '/community',
+							type: 'inbound',
+						},
+					]}
+				/>
 				<div className={themeClass}>{children}</div>
 			</Min100Layout>
 			<ConsentManager />
 		</>
 	)
+}
+
+VagrantIoLayout.rivetParams = {
+	query,
+	dependencies: [],
 }
 
 export default VagrantIoLayout
